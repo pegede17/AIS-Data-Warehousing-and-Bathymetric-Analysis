c65f66d64382dd82627da23e53f75145506da2f4
pegede17 <pegede17@student.aau.dk>
2021-09-15 12:10:21 +0200

    Improved create database script

--- create_database.py
+++ create_database.py
@@ -1,12 +1,31 @@
 import psycopg2
 import pygrametl
+import configparser
 
-dw_string = "host='localhost' dbname='postgres' user='postgres' password='admin'"
+config = configparser.ConfigParser()
+config.read('application.properties')
+
+dw_string = "host='{}' dbname='postgres' user='{}' password='{}'".format(
+    config["Database"]["hostname"],
+    config["Database"]["dbuser"],
+    config["Database"]["dbpass"],
+)
 connection = psycopg2.connect(dw_string)
 dw_conn_wrapper = pygrametl.ConnectionWrapper(connection=connection)
 
 cur = connection.cursor()
-cur.execute('CREATE DATABASE p9-test;')
+cur.execute("CREATE DATABASE {};".format(config["Database"]["dbname"]))
+cur.close()
+connection.close()
+
+dw_string = "host='{}' dbname='{}' user='{}' password='{}'".format(
+    config["Database"]["hostname"],
+    config["Database"]["dbname"],
+    config["Database"]["dbuser"],
+    config["Database"]["dbpass"],
+)
+connection = psycopg2.connect(dw_string)
+cur = connection.cursor()
 cur.execute("CREATE EXTENSION postgis;")
 cur.close()
 connection.close()
