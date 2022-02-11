19d6d367d933a36b262f150537c7d0484e3d8afc
Ali <aliozturk@outlook.dk>
2022-02-11 10:09:54 +0100

    Created main file with a new way of iterating dates

--- main.py
+++ main.py
@@ -7,169 +7,11 @@ from reverse_file import reverse_file
 # from create_trajectories import create_trajectories
 # import resource
 import gc
+from dateutil import rrule
+from datetime import datetime, timedelta
 
 config = configparser.ConfigParser()
 config.read('application.properties')
-
-# listOfDates = [20211001]
-
-# dates = [
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "01"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "02"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "03"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "04"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "05"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "06"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "07"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "08"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "09"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "10"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "11"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "12"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "13"
-#     # },
-#     # {
-#     #     "year": "2021",
-#     #     "month": "10",
-#     #     "date": "14"
-#     # },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "15"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "16"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "17"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "18"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "19"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "20"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "21"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "22"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "23"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "24"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "25"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "26"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "27"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "28"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "29"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "30"
-#     },
-#     {
-#         "year": "2021",
-#         "month": "10",
-#         "date": "31"
-#     }
-# ]
 
 # Data loading
 
@@ -191,28 +33,46 @@ config.read('application.properties')
 # print("Finished " + date["year"] + date["month"] + date["date"])
 # gc.collect(generation=2)
 
-for month in range(11):
-    for day in range(31):
-        if(month == 1 and day > 27):
-            continue
-        if(month == 3 or month == 5 or month == 8 or month == 10):
-            if(day == 30):
-                continue
+# for month in range(11):
+#     for day in range(31):
+#         if(month == 1 and day > 27):
+#             continue
+#         if(month == 3 or month == 5 or month == 8 or month == 10):
+#             if(day == 30):
+#                 continue
 
-        # file = f'aisdk_2021{(month + 1):02d}{(day + 1):02d}.csv'
-        date = "2021" + f'{(month + 1):02d}' + f'{(day + 1):02d}'
-        # # print("Reversing: " + file)
-        # # reverse_file(file)
-        # config["Environment"]["FILE_NAME"] = "r_" + file
-        # print(config["Environment"]["FILE_NAME"])
-        # print("2021" + f'{(month + 1):02d}' + f'{(day + 1):02d}')
-        clean_data(config, "2021" + f'{(month + 1):02d}' + f'{(day + 1):02d}')
-        # print("Loading: " + file)
-        # load_data_into_db(config)
-        # create_trajectories(date, config)
-        print("Finished " + str(date))
-        gc.collect(generation=2)
-        # config["Database"]["initialize"] = "False"
+#         # file = f'aisdk_2021{(month + 1):02d}{(day + 1):02d}.csv'
+#         date = "2021" + f'{(month + 1):02d}' + f'{(day + 1):02d}'
+#         print(date)
+#         # # print("Reversing: " + file)
+#         # # reverse_file(file)
+#         # config["Environment"]["FILE_NAME"] = "r_" + file
+#         # print(config["Environment"]["FILE_NAME"])
+#         # print("2021" + f'{(month + 1):02d}' + f'{(day + 1):02d}')
+#         # clean_data(config, "2021" + f'{(month + 1):02d}' + f'{(day + 1):02d}')
+#         # print("Loading: " + file)
+#         # load_data_into_db(config)
+#         # create_trajectories(date, config)
+#         print("Finished " + str(date))
+#         gc.collect(generation=2)
+#         # config["Database"]["initialize"] = "False"
+
+start_date = datetime(2021,1,1)
+end_date = datetime(2021,12,31)
+
+# Creates a list based on a frequency basis. E.g. Daily = [start_date, 11-02-2022, 12-02-2022 ... end_date] (inclusive)
+listOfDates = rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date)
+
+for date in listOfDates:
+    dateFormatted = int(date.strftime("%Y%m%d"))
+    print(f"Currently processing date: {dateFormatted}")
+
+    clean_data(config, dateFormatted)
+
+    print(f"Finished processing date: {dateFormatted}")
+    gc.collect(generation=2)
+
+
 
 # Trajectory Creation
 
