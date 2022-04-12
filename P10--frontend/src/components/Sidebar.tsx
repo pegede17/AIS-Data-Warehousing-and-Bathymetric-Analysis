a77d27ec8e691a7c2d556b4bc6ec9c043e42e50c
Ali <aliozturk@outlook.dk>
2022-04-12 15:09:25 +0200

    Fixed css on header in sidebar

--- P10--frontend/src/components/Sidebar.tsx
+++ P10--frontend/src/components/Sidebar.tsx
@@ -6,25 +6,25 @@ import '../styles/hideOrShowSidebar.scss';
 import '../styles/datePicker.scss';
 import ListButton from './ListButton';
 import * as muiSidebarStyling from '../styles/muiSidebarStyling';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {DesktopDatePicker} from '@mui/x-date-pickers/DesktopDatePicker';
 import TextField from '@mui/material/TextField';
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
-import { IconButton } from '@mui/material';
+import {Container, Grid, IconButton, Typography} from '@mui/material';
 
 const Sidebar: React.FC = () => {
     const {isShown, handleSidebar} = React.useContext(SidebarContext);
     const [fromDate, setFromDate] = React.useState<Date | null>(
-      new Date('2021-01-01T12:00:00'),
+        new Date('2021-01-01T12:00:00'),
     );
     const [toDate, setToDate] = React.useState<Date | null>(
-      new Date('2021-01-07T12:00:00'),
+        new Date('2021-01-07T12:00:00'),
     );
 
     const shipListName = "Ship Types";
     const shipList = ["Sailing", "Pleasure", "Cargo", "Passenger", "Military"];
-    
+
     const aisListName = "AIS Transponder Type";
     const aisTypes = ["Type A", "Type B"];
 
@@ -33,49 +33,58 @@ const Sidebar: React.FC = () => {
 
     const handleFromDate = (newValue: Date | null) => {
         setFromDate(newValue);
-      };
+    };
     const handleToDate = (newValue: Date | null) => {
-      setToDate(newValue);
+        setToDate(newValue);
     };
 
     // Old classname 'bg-black text-white sidebar
     return (
         <SidebarContainer className={'bg-white text-black sidebar'}>
-            <div className={'hide-or-show-sidebar'}>
-              <IconButton sx={muiSidebarStyling.ExpandButtonStyle}>
-                <DoubleArrowIcon onClick={() => handleSidebar()}/> 
-              </IconButton>
-            </div>
-            <div style={{margin: '1.2rem'}}>
-              <h2 style={{color: '#4f7ffe', textAlign: 'center'}}>Draught Overview</h2>
-              <div className='date-picker'>
-                <LocalizationProvider dateAdapter={AdapterMoment}>
-                    <DesktopDatePicker
-                        label="From:"
-                        inputFormat="DD/MM/yyyy"
-                        value={fromDate}
-                        onChange={handleFromDate}
-                        renderInput={(params) => <TextField {...params} />}
-                        />
-                    <DesktopDatePicker
-                        label="To:"
-                        inputFormat="DD/MM/yyyy"
-                        value={toDate}
-                        onChange={handleToDate}
-                        renderInput={(params) => <TextField {...params} />}
-                        />
-                </LocalizationProvider>
-              </div>
 
-              <ListButton listItems={shipList} listName={shipListName}/>
-              <ListButton listItems={aisTypes} listName={aisListName}/>
-              <ListButton listItems={gridList} listName={gridListName}/>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item xs={10}>
+                        <Typography variant={'h6'} sx={{py: 4, fontWeight: 'bold', color: '#4f7ffe'}}>Draught
+                            Overview</Typography>
+                    </Grid>
+                    <Grid item xs={2} sx={{mt: 1}}>
+                        <IconButton sx={muiSidebarStyling.ExpandButtonStyle}>
+                            <DoubleArrowIcon onClick={() => handleSidebar()}/>
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Container>
 
-              <div style={{display: "flex", justifyContent: "space-evenly"}}>
-                  <Button sx={{py: 1.5, px: 3, ...muiSidebarStyling.buttonRevertStyle}}>Revert</Button>
-                  <Button sx={{py: 1.5, px: 3, ...muiSidebarStyling.buttonApplyStyle}}>Apply</Button>
-              </div>
-            </div>
+            <Container>
+                <div className='date-picker'>
+                    <LocalizationProvider dateAdapter={AdapterMoment}>
+                        <DesktopDatePicker
+                            label="From:"
+                            inputFormat="DD/MM/yyyy"
+                            value={fromDate}
+                            onChange={handleFromDate}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                        <DesktopDatePicker
+                            label="To:"
+                            inputFormat="DD/MM/yyyy"
+                            value={toDate}
+                            onChange={handleToDate}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                </div>
+
+                <ListButton listItems={shipList} listName={shipListName}/>
+                <ListButton listItems={aisTypes} listName={aisListName}/>
+                <ListButton listItems={gridList} listName={gridListName}/>
+
+                <div style={{display: "flex", justifyContent: "space-evenly"}}>
+                    <Button sx={{py: 1.5, px: 3, ...muiSidebarStyling.buttonRevertStyle}}>Revert</Button>
+                    <Button sx={{py: 1.5, px: 3, ...muiSidebarStyling.buttonApplyStyle}}>Apply</Button>
+                </div>
+            </Container>
         </SidebarContainer>
     );
 };
