54153b7f56270c7d7eb48319dbf091331b91a774
pegede17 <pegede17@student.aau.dk>
2022-04-26 09:43:27 +0200

    raster creation

---
+++ P10--backend/raster_creation.py
@@ -0,0 +1,63 @@
+from osgeo import gdal
+from osgeo import osr
+import numpy as np
+import os
+import sys
+from pygrametl.datasources import CSVSource, SQLSource
+from database_connection import connect_to_local, connect_via_ssh
+
+ais_file_handle = open(
+    'C:/Users/Peter/Desktop/data-1650875642354 - mindre.csv')
+csv_source = CSVSource(f=ais_file_handle, delimiter=',')
+
+connection = connect_via_ssh()
+
+query = """
+        SELECT columnx_50m, rowy_50m, CASE WHEN draught is null then -1 else draught END from 
+        dim_cell d inner join 
+        (SELECT cell_id, max(max_draught) draught, sum(trajectory_count)trajectory_count
+	    FROM fact_cell
+	    GROUP BY cell_id
+        LIMIT 5000) f
+	    on f.cell_id = d.cell_id
+        """
+
+sql_source = SQLSource(connection=connection, query=query)
+
+#  Initialize the Image Size
+image_size = (16000, 22000)
+
+draughts = np.zeros((image_size), dtype=np.float32)
+
+
+for row in csv_source:
+    draughts[int(row["rowy_50m"]), int(
+        row["columnx_50m"])] = float(row['draught'])
+
+draughts[1, 1] = 29.2
+
+#  Create Each Channel
+
+# set geotransform
+nx = image_size[0]
+ny = image_size[1]
+xmin, ymin, xmax, ymax = [0, 6700000, 1100000, 5900000]
+xres = 50
+yres = 50
+geotransform = (xmin, xres, 0, ymax, 0, yres)
+
+# create the 3-band raster file
+dst_ds = gdal.GetDriverByName('GTiff').Create(
+    'draughts_debug_csv_5000_v5.tif', ny, nx, 1, gdal.GDT_Float32)
+
+dst_ds.SetGeoTransform(geotransform)    # specify coords
+srs = osr.SpatialReference()            # establish encoding
+srs.ImportFromEPSG(32632)                # WGS84 lat/long
+dst_ds.SetProjection(srs.ExportToWkt())  # export coords to file
+dst_ds.GetRasterBand(1).SetNoDataValue(0)
+dst_ds.GetRasterBand(1).WriteArray(draughts)
+# dst_ds.GetRasterBand(1).WriteArray(r_pixels)   # write r-band to the raster
+# dst_ds.GetRasterBand(2).WriteArray(g_pixels)   # write g-band to the raster
+# dst_ds.GetRasterBand(3).WriteArray(b_pixels)   # write b-band to the raster
+dst_ds.FlushCache()                     # write to disk
+dst_ds = None
