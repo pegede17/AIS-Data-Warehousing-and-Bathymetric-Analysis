1b27e4ccef49005fcc07cf8d186eed68310ff119
pegede17 <pegede17@student.aau.dk>
2022-05-23 11:40:37 +0200

    Fix CORS actually this time

--- P10--frontend/src/utils/API.tsx
+++ P10--frontend/src/utils/API.tsx
@@ -7,7 +7,7 @@ import qs from "qs";
 export default {
     map: {
         getBoxes: async (params: RasterRequestParameters) => {
-            return await axios.get<FeatureCollection>('http://127.0.0.1:5000/boxes', {
+            return await axios.get<FeatureCollection>('http://130.225.39.233:5000/boxes', {
                 params,
                 paramsSerializer: params => {
                     return qs.stringify(params, {arrayFormat: 'comma'})
@@ -15,7 +15,7 @@ export default {
             });
         },
         getHistogram: async (params: HistogramParameters) => {
-            return await axios.get<number[]>('http://127.0.0.1:5000/histogram', {
+            return await axios.get<number[]>('http://130.225.39.233:5000/histogram', {
                 params,
                 paramsSerializer: params => {
                     return qs.stringify(params, {arrayFormat: 'comma'})
