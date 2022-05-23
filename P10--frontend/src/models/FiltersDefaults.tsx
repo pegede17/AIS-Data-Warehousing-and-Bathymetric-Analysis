217027dd899e3ed9a91162fc670314b21a79339c
pegede17 <pegede17@student.aau.dk>
2022-05-23 14:00:05 +0200

    Correct filters

--- P10--frontend/src/models/FiltersDefaults.tsx
+++ P10--frontend/src/models/FiltersDefaults.tsx
@@ -1,6 +1,32 @@
 import {GradientColors} from "../hooks/useMapDetails";
 
-export const shipList = ["Unknown","Other","Passenger","Cargo","Pilot","Tanker","Sailing"];
+export const shipList = ["Unknown",
+"Other",
+"Passenger",
+"Cargo",
+"Pilot",
+"Tanker",
+"Sailing",
+"Fishing",
+"Dredging",
+"SAR",
+"Undefined",
+"HSC",
+"Tug",
+"Port tender",
+"Reserved",
+"Not party to conflict",
+"Military",
+"Law enforcement",
+"Pleasure",
+"Diving",
+"Towing long/wide",
+"Anti-pollution",
+"Towing",
+"Medical",
+"Spare 2",
+"Spare 1",
+"WIG"];
 export const aisTypes = ["Unknown","Class A","Class B","Base Station","AtoN","SAR Airborne","Search and Rescue Transponder"];
 export const trustedDraughts = ["Only trusted draughts"];
 export const defaultGradientColors: GradientColors = {
